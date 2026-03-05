c2b535d4a34f60fe3a32b9ddf7baeaec8cbc9d80
Tanaka Makuvaza <tmaku12430@gmail.com>
2026-03-04 19:00:50 -0500

    feat: add HomeScreen with recipe list and navigation to Details

---
+++ O_CW/CW02/recipe_book_app/lib/screens/details_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../models/recipe.dart';
+
+class DetailsScreen extends StatelessWidget {
+  final Recipe recipe;
+
+  const DetailsScreen({super.key, required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(recipe.name)),
+      body: const Center(child: Text('Recipe details')),
+    );
+  }
+}
